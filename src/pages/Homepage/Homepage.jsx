ac800d571511d267583b490b5b52f6a1373f6592
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-11-24 21:37:01 +0530

    Background added

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.scss'
+import Directory from '../../components/Directory/Directory';
+
+const Homepage = () => {
+	return (
+		<div className='homepage'>
+			<Directory />
+		</div>
+	);						 
+}
+
+export default Homepage;
