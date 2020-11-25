b49de44e8c508f469550a8c5be692e1b45e53390
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-11-25 20:42:34 +0530

    Added react router functionality

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './MenuItem.styles.scss';
 import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
 
 	return (
 		<div 
