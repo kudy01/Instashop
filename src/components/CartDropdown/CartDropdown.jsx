ed1285dc905a31c3812438c671874c02d005a5c6
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-12-11 19:20:55 +0530

    Cart item created which displays items in the dropdown

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
 import CustomButton from '../CustomButton/CustomButton'; 
+import CartItem from '../CartItem/CartItem';
+import { connect } from 'react-redux';
 
 import './CartDropdown.styles.scss';
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
 
 	return (
 		<div className='cart-dropdown'>
 			<div className='cart-items'>
+				{
+					cartItems.map((cartItem, i) => {
+						return <CartItem key={i} item={cartItem} />
+					})
+				}
 			</div>
 			<CustomButton>GO TO CHECKOUT </CustomButton>
 		</div>
 	)
 }
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+	cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
+
