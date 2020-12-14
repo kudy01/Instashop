73a6c8e9f68dba78e38fa1e0d93247d5b3e1761d
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-12-14 12:53:53 +0530

    Added a feature to toggle the cart dropdown off, when going to checkout page

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -4,12 +4,13 @@ import CustomButton from '../CustomButton/CustomButton';
 import CartItem from '../CartItem/CartItem';
 import { connect } from 'react-redux';
 import { selectCartItems } from '../../redux/cart/cart.selector';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { withRouter } from 'react-router-dom';
 
 import './CartDropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
 
 	return (
 		<div className='cart-dropdown'>
@@ -23,7 +24,11 @@ const CartDropdown = ({ cartItems, history }) => {
 					<span className='empty-message'>Your cart is empty</span>
 				}
 			</div>
-			<CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT </CustomButton>
+			<CustomButton onClick={() => {
+				history.push('/checkout');
+				dispatch(toggleCartHidden())
+			}}
+				>GO TO CHECKOUT </CustomButton>
 		</div>
 	)
 }
