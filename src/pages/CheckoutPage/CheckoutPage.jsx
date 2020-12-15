4aa13a138de2be74a84b994c8f454a821c4d43f4
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-12-15 21:39:41 +0530

    Added session management with redux persit

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector';
+import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
 
 import './CheckoutPage.styles.scss';
 
@@ -29,7 +30,7 @@ const CheckoutPage = ({ cartItems, total }) => {
 			</div>
 			{
 				cartItems.map(cartItem => {
-					return cartItem.name
+					return <CheckoutItem key={cartItem.id} cartItem={cartItem} />
 				})
 			}
 			<div className='total'>
