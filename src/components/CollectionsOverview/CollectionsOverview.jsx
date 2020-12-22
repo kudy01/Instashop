44055d65115b6b5cebd58188f1692d911ba08595
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-12-22 19:17:00 +0530

    Renamed files and fixed errors due to duplication of code from last commit

---
+++ src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
+
+import CollectionPreview from '../CollectionPreview/CollectionPreview';
+
+// import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+
+import './CollectionsOverview.styles.scss';
+
+const CollectionsOverview = () => (
+  <div className='collections-overview'>
+    check
+  </div>
+);
+
+// const mapStateToProps = createStructuredSelector({
+//   collections: selectCollectionsForPreview
+// });
+
+export default (CollectionsOverview);
