57acb8655389a2ea0d002094ccb936863eb15c15
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-12-23 14:17:11 +0530

    Fixing the collections overview component so that proper items are displayed in the shop route

--- src/components/CollectionsOverview/CollectionsOverview.jsx
+++ src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../CollectionPreview/CollectionPreview';
 
-// import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shopdata/shop.selector';
 
 import './CollectionsOverview.styles.scss';
 
-const CollectionsOverview = () => (
+const CollectionsOverview = ({ collections }) => (
   <div className='collections-overview'>
-    check
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
   </div>
 );
 
-// const mapStateToProps = createStructuredSelector({
-//   collections: selectCollectionsForPreview
-// });
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview
+});
 
-export default (CollectionsOverview);
+export default connect(mapStateToProps)(CollectionsOverview);
