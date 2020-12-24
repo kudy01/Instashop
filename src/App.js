d8768305406d3d37edf5242ea4892cc705297794
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-12-24 14:44:43 +0530

    Fixed bugs on deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
 import Header from './components/Header/Header';
