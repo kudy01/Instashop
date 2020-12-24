4311d92af240ed811d8692786e6c6a4a1d8ab22c
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2020-12-24 17:17:45 +0530

    Removing logger when in production stage

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,9 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [logger];
 
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
 	middlewares.push(logger)
 }
 
