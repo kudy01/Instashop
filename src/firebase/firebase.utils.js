c337d1cd0815335c266a8f5a1fa76e784c444b15
Kushal Jaiswal <68225773+kudy01@users.noreply.github.com>
2021-01-07 14:35:10 +0530

    Added a spinner component, made the collectionRef call into a promise, fixed display of shop page

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -53,20 +53,23 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   	return await batch.commit();
 }
 
-export const convertCollectionsSnapshotToMap = ( collections => {
-	const transformedCollection = collections.docs.map(doc => {
-		const { title, items } = doc.data()
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
 
-		return {
-			routeName: encodeURI(title.toLowerCase()),
-			id: doc.id,
-			title, 
-			items
-		}
-	})
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
 
-	console.log(transformedCollection)
-})
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 firebase.initializeApp(config);
 
--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -4,30 +4,52 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview';
 import CollectionPage from '../CollectionPage/CollectionPage';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shopdata/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 class ShopPage extends React.Component {
 	
+	state = {
+		loading: true 
+	}
 	unsubscribeFromSnapShot = null;
 
 	componentDidMount = () => {
+		const { updateCollections } = this.props;
+
 		const collectionRef = firestore.collection('collections');
 
-		collectionRef.onSnapshot( async snapShot => {
-			convertCollectionsSnapshotToMap(snapShot)
+		collectionRef.get().then(snapShot => {
+			const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
+			console.log('ss',collectionsMap)
+			updateCollections(collectionsMap);
+			this.setState({ loading: false });
 		})
+		
 	}
 
 	render() {
 		const { match } = this.props;
+		const { loading } = this.state;
 
 		return (
 			<div className='shop-page'>
-		    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-		    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+		    <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>}/>
+		    <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props}/>} />
 		  </div>
 		)
 	}
   
 }
 
-export default ShopPage;
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+
+export default connect(null, mapDispatchToProps)(ShopPage);
